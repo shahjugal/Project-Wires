6e38fb8f8b5b0e081ce8b01bb0b885aa6c8064dd
Jugal <23151283+shahjugal@users.noreply.github.com>
2023-08-28 16:59:49 +0530

    fixed deleting of tweets when author deletes account

--- Project-Wires/Models/Tweet.py
+++ Project-Wires/Models/Tweet.py
@@ -6,9 +6,8 @@ class Tweet(Base):
     __tablename__ = "tweets"
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     content = Column(String, nullable=False)
-    author_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    author_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     parent_tweet_id = Column(Integer, ForeignKey("tweets.id"), nullable=True, default=text('NULL'))
     created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
     modified_at = Column(TIMESTAMP(timezone=True), server_default=text('now()') , nullable=False)
-    
     
