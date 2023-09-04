5f8c808f4bad90b1ac3302413c3de141ed1de3c7
Jugal <23151283+shahjugal@users.noreply.github.com>
2023-09-04 17:02:22 +0530

    added preview apis

--- Project-Wires/Enpoints/AuthenticationEP.py
+++ Project-Wires/Enpoints/AuthenticationEP.py
@@ -1,5 +1,5 @@
 from typing import  Optional
-from fastapi import APIRouter, Body, Depends, HTTPException
+from fastapi import APIRouter, Body, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 from Models import User
 from PyDanticModels import PasswordResetInputModel, PasswordResetOutputModel, RegisterInputModel, RegistrationOutputModel, LoginInputModel, LoginOutputModel, Secret2FAOutputModel, twoFAInputModel
@@ -105,3 +105,31 @@ def disable_2FA(db: Session = Depends(get_db), user_id: str = Depends(get_curren
     except Exception as e:
         # If another exception occurs, raise a custom HTTPException with a 500 status code
         raise HTTPException(status_code=500, detail=str(e))
+    
+@router.get("/user/forgotten-password/", tags=['[Authentication]Coming Soon'])
+def get_password_reset_mail(db: Session = Depends(get_db), ):
+    """
+    This will soon send mail to user with their password reset link.
+    """
+    pass
+
+@router.post("/user/forgotten-password/", tags=['[Authentication]Coming Soon'])
+def set_password_using_hex(db: Session = Depends(get_db), hex_code: str = Query(...), password: str = Body(...)):
+    """
+    This will allow to reset user password with given hex code and new password if its valid and not expired.
+    """
+    pass
+
+@router.get("/user/verify-account/", tags=['[Authentication]Coming Soon'])
+def get_account_verification_mail(db: Session = Depends(get_db), user_id: str = Depends(get_current_user),):
+    """
+    This will soon send mail to user to verify their account.
+    """
+    pass
+
+@router.post("/user/verify-account/", tags=['[Authentication]Coming Soon'])
+def veridy_account_using_hex(db: Session = Depends(get_db), hex_code: str = Query(...)):
+    """
+    This will allow to verify account with hex code if its valid and not expired.
+    """
+    pass
